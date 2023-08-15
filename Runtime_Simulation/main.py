7a7268393a702fc3fb7232c178d3dc742130b40a
billyen33 <billmembers33@gmail.com>
2023-08-15 15:40:53 +0800

    add comments

--- Runtime_Simulation/main.py
+++ Runtime_Simulation/main.py
@@ -17,31 +17,31 @@ avg_power_v3 = 1e6*(avg_v3**2)/2000 #2k load, power in uW
 print("Avg. v0: " + str(1000*avg_v0) + " mV" , str(avg_power_v0) + " uW")
 print("Avg. v3: " + str(1000*avg_v3) + " mV" , str(avg_power_v3) + " uW")
 
-MARS_on_0 = []
+Analog_on_0 = []
 for v in v0_avg_v:
     if v > 0.2:
-        MARS_on_0.append(1)
+        Analog_on_0.append(1)
     else:
-        MARS_on_0.append(0)
+        Analog_on_0.append(0)
 
-MARS_on_3 = []
+Analog_on_3 = []
 for v in v3_avg_v:
     if v > 0.2:
-        MARS_on_3.append(1)
+        Analog_on_3.append(1)
     else:
-        MARS_on_3.append(0)
+        Analog_on_3.append(0)
 
 #Call simulate function
 C0 = [0.007000000000000006, 0.007000000000000006, 0.007000000000000006]
 C3 = [0.007000000000000006, 0.007000000000000006, 0.007000000000000006]
-Ambiq0, MSP430_0, ignore = models.simulate(days, v0_avg_v, C0)
-Ambiq3, MSP430_3, ignore = models.simulate(days, v3_avg_v, C3)
+Advanced0, Minimal_0, ignore = models.simulate(days, v0_avg_v, C0)
+Advanced3, Minimal_3, ignore = models.simulate(days, v3_avg_v, C3)
 
-print(sum(Ambiq3)/sum(Ambiq0))
-print(sum(MSP430_3)/sum(MSP430_0))
-print(sum(MARS_on_3)/sum(MARS_on_0))
+print(sum(Advanced3)/sum(Advanced0))
+print(sum(Minimal_3)/sum(Minimal_0))
+print(sum(Analog_on_3)/sum(Analog_on_0))
 
 #generate graphs to visualize data below:
-visualizations.bar_subplots_mars(days, MARS_on_0, MARS_on_3)
-visualizations.bar_subplots2(Ambiq0, Ambiq3, MSP430_0, MSP430_3) # generate graph 13
-visualizations.bar_subplots(Ambiq3, MSP430_3) # generate graph 2
+visualizations.bar_subplots_analog(days, Analog_on_0, Analog_on_3)
+visualizations.bar_subplots2(Advanced0, Advanced3, Minimal_0, Minimal_3) # generate graph 13
+visualizations.bar_subplots(Advanced3, Minimal_3) # generate graph 2
--- Runtime_Simulation/models.py
+++ Runtime_Simulation/models.py
@@ -67,7 +67,7 @@ def update_capEnergy(e0, V_applied, R, C, dt):
     
     return e_cap, v_cap #output final e and v
 
-def Ambiq_energy():
+def Advanced_energy():
     #Now representing "Advanced"
     #startup time of 2500 ms
     t = 2500e-3
@@ -75,14 +75,14 @@ def Ambiq_energy():
     e_startup = 2.4 * 128e-3 * 5e-3
     return e+e_startup
 
-def MSP430_energy():
+def Minimal_energy():
     #Now representing "Minimal"
     t = 0.888e-3 #tentative time
-    e = 0.9 * 12.4e-3 * t
+    e = 0.9 * 4.8e-3 * t #this uses average current
     e_startup = 0#assume negligible, no known startup time given
     return  e + e_startup
 
-def MARS_energy():
+def Analog_energy():
     #Now representing Analog
     t = 1e-3 #estimated operating time
     e = 0.11 * 2.15e-6 * t
@@ -91,12 +91,12 @@ def MARS_energy():
 
 #STEP 3:
 # For each day:
-#   on_MSP430, on_Ambiq, on_MARS = 0
+#   on_Minimal, on_Advanced, on_Analog = 0
 #   For each time step (like every 60 s given our logging freq):
 #       - Update the energy in our capacitor (put fcn in models.py) given (1) input voltage, (2) time step, (3) capacitance (prob 10 uF), this will be an integral
 #       - Check if energy is enough to turn on (1) 1 uJ load, (2) 10 uJ load, and (3) 20 uJ load (will tweak later to reflect real energy cost of each system)
-#       - If so, add to on_MSP430, on_Ambiq, and on_MARS and reset capacitor energy to 0 J (might tweak this value)
-#   Append on_MSP430, on_Ambiq, on_MARS to on_MSP430_list, on_Ambiq_list, on_MARS_list. This will be a list of how many sensor readings we are able to take with each of these systems every day given the energy we got
+#       - If so, add to on_Minimal, on_Advanced, and on_Analog and reset capacitor energy to 0 J (might tweak this value)
+#   Append on_Minimal, on_Advanced, on_Analog to on_Minimal_list, on_Advanced_list, on_Analog_list. This will be a list of how many sensor readings we are able to take with each of these systems every day given the energy we got
 #STEP 4: Visualize the daily # of readings with 3 bar graphs, y axis is # of readings and x axis is days.
 #   - Given 3 lists of integer values, plot them on bar graphs
 
@@ -104,27 +104,27 @@ def simulate(t_list, v_list, C_h):
     # t_list: list of decimal time stamps in unit of days (e.g. 71.85893518518519 day), same length as v_list
     # v_list: list of voltage values from SFMC
     # C_h: capacitance of the capacitor being filled up by harvester
-    on_Ambiq_list = []
-    on_MARS_list = []
-    on_MSP430_list = []
+    on_Advanced_list = []
+    on_Analog_list = []
+    on_Minimal_list = []
 
     #assume capacitor is completely discharged at start
-    e_ambiq_init = 0 
-    e_msp430_init = 0
-    e_mars_init = 0
+    e_advanced_init = 0 
+    e_minimal_init = 0
+    e_analog_init = 0
 
     #Initialize daily sensor reading count
-    on_Ambiq = 0
-    on_MSP430 = 0
-    on_MARS = 0
+    on_Advanced = 0
+    on_Minimal = 0
+    on_Analog = 0
 
-    cap_energy_mars = []
-    cap_energy_msp430 = []
-    cap_energy_ambiq = []
+    cap_energy_analog = []
+    cap_energy_minimal = []
+    cap_energy_advanced = []
 
-    cap_v_mars = []
-    cap_v_msp430 = []
-    cap_v_ambiq = []
+    cap_v_analog = []
+    cap_v_minimal = []
+    cap_v_advanced = []
 
     #for each voltage data point
     for jj in range(1,len(t_list)): #last data point was at 71.85893518518519 day
@@ -132,74 +132,72 @@ def simulate(t_list, v_list, C_h):
         t = 24*60*60*(t_list[jj] - t_list[jj-1]) #dt is time since last measurement in seconds
 
         #update amount of energy in capacitor given v0 output
-        E_Ambiq, v_ambiq = update_capEnergy(e_ambiq_init, V_applied=v_list[jj], R=internal_R_v0(), C=C_h[0], dt=t)
-        E_MSP430, v_msp430 = update_capEnergy(e_msp430_init, V_applied=v_list[jj], R=internal_R_v0(), C=C_h[0], dt=t)
-        E_MARS, v_mars = update_capEnergy(e_mars_init, V_applied=v_list[jj], R=internal_R_v0(), C=C_h[0], dt=t)
+        E_Advanced, v_advanced = update_capEnergy(e_advanced_init, V_applied=v_list[jj], R=internal_R_v0(), C=C_h[0], dt=t)
+        E_Minimal, v_minimal = update_capEnergy(e_minimal_init, V_applied=v_list[jj], R=internal_R_v0(), C=C_h[0], dt=t)
+        E_Analog, v_analog = update_capEnergy(e_analog_init, V_applied=v_list[jj], R=internal_R_v0(), C=C_h[0], dt=t)
         
         #Check if we have enough power to turn things on
-        if E_Ambiq > Ambiq_energy():
-            on_Ambiq = on_Ambiq + round(E_Ambiq/Ambiq_energy())
-            E_Ambiq = 0 #completely discharge, prob bad assumption will change based on matrix board stat
-            v_ambiq = 0
+        if E_Advanced > Advanced_energy():
+            on_Advanced = on_Advanced + round(E_Advanced/Advanced_energy())
+            E_Advanced = 0 #completely discharge, prob bad assumption will change based on matrix board stat
+            v_advanced = 0
             
-        if E_MSP430 > MSP430_energy():
-            on_MSP430 = on_MSP430 + round(E_MSP430/MSP430_energy())
-            E_MSP430 = 0 #completely discharge, prob bad assumption will change based on matrix board stat
-            v_msp430 = 0
+        if E_Minimal > Minimal_energy():
+            on_Minimal = on_Minimal + round(E_Minimal/Minimal_energy())
+            E_Minimal = 0 #completely discharge, prob bad assumption will change based on matrix board stat
+            v_minimal = 0
             
-        if E_MARS > MARS_energy():
-            #print(E0_MARS, models.MARS_energy(), E0_MARS/models.MARS_energy())
-            on_MARS = on_MARS + round(E_MARS/MARS_energy())
-            
-            E_MARS = 0 #completely discharge, prob bad assumption will change based on matrix board stat
-            v_mars = 0
+        if E_Analog > Analog_energy():
+            on_Analog = on_Analog + round(E_Analog/Analog_energy())
+            E_Analog = 0 #completely discharge, prob bad assumption will change based on matrix board stat
+            v_analog = 0
 
-        cap_energy_mars.append(E_MARS)
-        cap_energy_msp430.append(E_MSP430)
-        cap_energy_ambiq.append(E_Ambiq)
-        cap_v_mars.append(v_mars)
-        cap_v_msp430.append(v_msp430)
-        cap_v_ambiq.append(v_ambiq)
+        cap_energy_analog.append(E_Analog)
+        cap_energy_minimal.append(E_Minimal)
+        cap_energy_advanced.append(E_Advanced)
+        cap_v_analog.append(v_analog)
+        cap_v_minimal.append(v_minimal)
+        cap_v_advanced.append(v_advanced)
 
         #update start condition for next loop
-        e_ambiq_init = E_Ambiq
-        e_msp430_init = E_MSP430
-        e_mars_init = E_MARS
+        e_advanced_init = E_Advanced
+        e_minimal_init = E_Minimal
+        e_analog_init = E_Analog
         
         if math.trunc(t_list[jj-1]) != math.trunc(t_list[jj]): #if a day ended
             #record the number of sensor reading that day to their respective lists
-            on_Ambiq_list.append(on_Ambiq)
-            on_MSP430_list.append(on_MSP430)
-            on_MARS_list.append(on_MARS)
+            on_Advanced_list.append(on_Advanced)
+            on_Minimal_list.append(on_Minimal)
+            on_Analog_list.append(on_Analog)
 
             #Reset daily sensor reading count
-            on_Ambiq = 0
-            on_MSP430 = 0
-            on_MARS = 0
+            on_Advanced = 0
+            on_Minimal = 0
+            on_Analog = 0
 
     # #Debugging print and plots
-    # '''print("# of readings by Ambiq: ", on_Ambiq_list)
-    # print("# of readings by MSP430: ", on_MSP430_list)
-    # print("# of readings by MARS: ", on_MARS_list)
+    # '''print("# of readings by Advanced: ", on_Advanced_list)
+    # print("# of readings by Minimal: ", on_Minimal_list)
+    # print("# of readings by Analog: ", on_Analog_list)
     # fig, axs = plt.subplots(3, 1, figsize=(12, 4), sharex=True)
-    # axs[0].plot(t_list[1:], cap_energy_ambiq, label="E in Ambiq Capacitor")
-    # axs[0].plot(t_list[1:], cap_energy_msp430, label="E in MSP430 Capacitor")
-    # axs[0].plot(t_list[1:], cap_energy_mars, label="E in MARS Capacitor")
+    # axs[0].plot(t_list[1:], cap_energy_advanced, label="E in Advanced Capacitor")
+    # axs[0].plot(t_list[1:], cap_energy_minimal, label="E in Minimal Capacitor")
+    # axs[0].plot(t_list[1:], cap_energy_analog, label="E in Analog Capacitor")
     # axs.flat[0].set(ylabel="Energy (J)")
-    # axs[1].plot(t_list[1:], cap_v_ambiq, label="V of Ambiq Capacitor")
-    # axs[1].plot(t_list[1:], cap_v_msp430, label="V of MSP430 Capacitor")
-    # axs[1].plot(t_list[1:], cap_v_mars, label="V of MARS Capacitor")
+    # axs[1].plot(t_list[1:], cap_v_advanced, label="V of Advanced Capacitor")
+    # axs[1].plot(t_list[1:], cap_v_minimal, label="V of Minimal Capacitor")
+    # axs[1].plot(t_list[1:], cap_v_analog, label="V of Analog Capacitor")
     # axs.flat[1].set(ylabel="Voltage (V)")
     # axs[2].plot(t_list[1:], v_list[1:], label="SMFC Voltage")
     # axs.flat[2].set(ylabel="SMFC Voltage (V)")
     # # specifying horizontal line type
-    # #plt.axhline(y = models.Ambiq_energy(), color = 'r', linestyle = '-')
-    # #plt.axhline(y = models.MARS_energy(), color = 'r', linestyle = '-.')
+    # #plt.axhline(y = models.Advanced_energy(), color = 'r', linestyle = '-')
+    # #plt.axhline(y = models.Analog_energy(), color = 'r', linestyle = '-.')
     # plt.xlabel("Timeline (Days)")
     # axs[0].legend()
     # axs[1].legend()'''
 
-    return on_Ambiq_list, on_MSP430_list, on_MARS_list
+    return on_Advanced_list, on_Minimal_list, on_Analog_list
 
 def getMax(c_list, input_list):
     max_value = max(input_list)
